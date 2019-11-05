cb53055414bd3089cc2f66b4fcd1d72e17d7e721
Vladyslav Chychykalo <vladyslav.chychykalo@gmail.com>
2019-11-05 21:30:03 +0200

    Updates

---
+++ precache-manifest.876446a4b23e3c5e1a94ee1f72273d07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ebe86e0432e7dbc8b7fdf6c65e6c3c3",
+    "url": "/goit-react-hw-02-bank-account/index.html"
+  },
+  {
+    "revision": "cf564ca9053f1d6bbb2e",
+    "url": "/goit-react-hw-02-bank-account/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "989063d35052ea510c99",
+    "url": "/goit-react-hw-02-bank-account/static/css/main.1945a511.chunk.css"
+  },
+  {
+    "revision": "cf564ca9053f1d6bbb2e",
+    "url": "/goit-react-hw-02-bank-account/static/js/2.c23f1f2f.chunk.js"
+  },
+  {
+    "revision": "989063d35052ea510c99",
+    "url": "/goit-react-hw-02-bank-account/static/js/main.b8ef394a.chunk.js"
+  },
+  {
+    "revision": "4456fcf01a6249bbb0da",
+    "url": "/goit-react-hw-02-bank-account/static/js/runtime-main.b10a97a6.js"
+  }
+]);
