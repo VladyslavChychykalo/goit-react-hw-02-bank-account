12f86ba53939b4ce913f005e154bcd5ae8282a7d
Vladyslav Chychykalo <vladyslav.chychykalo@gmail.com>
2019-12-05 19:46:57 +0200

    mod

--- src/components/Dashboard/Controls/Controls.jsx
+++ src/components/Dashboard/Controls/Controls.jsx
@@ -1,7 +1,4 @@
 import React, { Component } from 'react';
-// import { ToastContainer, toast } from 'react-toastify';
-// import 'react-toastify/dist/ReactToastify.css';
-// import shortid from 'shortid';
 import PropTypes from 'prop-types';
 import styles from './Controls.module.css';
 
